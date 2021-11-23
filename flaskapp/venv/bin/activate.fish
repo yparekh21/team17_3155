236c0e2719d11db3856707eeac21b265ba4f21c8
Ioyorol <89850701+Ioyorol@users.noreply.github.com>
2021-11-23 08:52:01 -0500

    venv install folder

--- flaskapp/venv/bin/activate.fish
+++ flaskapp/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jad/team17_3155/flaskapp/venv"
+set -gx VIRTUAL_ENV "/Users/jad/Desktop/Team17_ITSC3155/team17_3155/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
